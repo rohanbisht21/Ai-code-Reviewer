9a33e9c3c86ab25f090bc7add6c14b2ee0bb26ff
Rohan Bisht <rohan.bisht@awfis.com>
2025-05-06 23:11:27 +0530

    added prismjs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import 'prismjs/themes/prism-tomorrow.css'
-import './App.css'
-import prism from "prismjs"
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "prismjs/themes/prism-tomorrow.css";
+import "./App.css";
+import prism from "prismjs";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
+  useEffect(() => {
+    prism.highlightAll();
+  });
   return (
     <>
       <main>
         <div className="left">
-          <div className="code"></div>
-          <div className="review"></div>
+          <div className="code">
+            <pre>
+              <code className="language-javascript">
+                {` function sum(){  
+return 1+1;
+                                } `}
+              </code>
+            </pre>
+          </div>
+          <div className="review">Review</div>
         </div>
         <div className="right"></div>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+function sum() {
+  return 1 + 1;
+}
+export default App;
